26e591e6abf63c595772f56b4eaafb95cdd6cc8e
Kirill Byvshev <k@byv.sh>
2025-02-22 02:02:00 +0400

    Separated screens

--- game/screens_manager/screens_manager.go
+++ game/screens_manager/screens_manager.go
@@ -5,36 +5,36 @@ components {
 embedded_components {
   id: "auth_factory"
   type: "collectionfactory"
-  data: "prototype: \"/game/auth_screen/auth_screen.collection\"\n"
+  data: "prototype: \"/screens/auth_screen/auth_screen.collection\"\n"
   ""
 }
 embedded_components {
   id: "lobby_factory"
   type: "collectionfactory"
-  data: "prototype: \"/game/lobby_screen/lobby_screen.collection\"\n"
+  data: "prototype: \"/screens/lobby_screen/lobby_screen.collection\"\n"
   ""
 }
 embedded_components {
   id: "match_factory"
   type: "collectionfactory"
-  data: "prototype: \"/game/match_screen/match_screen.collection\"\n"
+  data: "prototype: \"/screens/match_screen/match_screen.collection\"\n"
   ""
 }
 embedded_components {
   id: "leaving_factory"
   type: "collectionfactory"
-  data: "prototype: \"/game/leaving_screen/leaving_screen.collection\"\n"
+  data: "prototype: \"/screens/leaving_screen/leaving_screen.collection\"\n"
   ""
 }
 embedded_components {
   id: "joining_factory"
   type: "collectionfactory"
-  data: "prototype: \"/game/joining_screen/joining_screen.collection\"\n"
+  data: "prototype: \"/screens/joining_screen/joining_screen.collection\"\n"
   ""
 }
 embedded_components {
   id: "ops_factory"
   type: "collectionfactory"
-  data: "prototype: \"/game/ops_screen/ops_screen.collection\"\n"
+  data: "prototype: \"/screens/ops_screen/ops_screen.collection\"\n"
   ""
 }
